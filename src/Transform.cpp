ff33d2917b9c0a70d74692969fa4e42e1d7ca3c5
mih <mihmin98@yahoo.com>
2020-05-18 16:05:02 +0300

    Created Transform class

---
+++ src/Transform.cpp
@@ -0,0 +1,9 @@
+#include "Transform.hpp"
+
+Transform::Transform(Vector3 position, Vector3 rotation, Vector3 scale)
+{
+    this->position = position;
+    this->rotation = rotation;
+    this->scale = scale;
+}
+
---
+++ src/Transform.hpp
@@ -0,0 +1,18 @@
+#ifndef _TRANSFORM_HPP_
+#define _TRANSFORM_HPP_
+
+#pragma once
+#include "Vector3.hpp"
+
+class Transform
+{
+  public:
+    Vector3 position;
+    Vector3 rotation;
+    Vector3 scale;
+
+    Transform(Vector3 position = Vector3(0, 0, 0), Vector3 rotation = Vector3(0, 0, 0),
+              Vector3 scale = Vector3(1, 1, 1));
+};
+
+#endif
